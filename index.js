605255e2cbca3924f86805fbafd82b13bfcca077
Jacob Garcia <jacobroxasgarcia@yahoo.com>
2022-05-28 22:23:19 -0700

    Adding 404 page route

--- index.js
+++ index.js
@@ -1,20 +1,13 @@
-require('dotenv').config();
-
-const express = require('express');
-const app = express();
-
-app.set('view engine', 'jsx')
-app.engine('jsx', require('express-react-views').createEngine())
-
-app.use('/places', require('./controllers/places'));
+require('dotenv').config()
+const express = require('express')
+const app = express()
 
 app.get('/', (req, res) => {
-    res.render ('home')
+    res.send('Hello world!')
 })
 
 app.get('*', (req, res) => {
-    res.render('error404')
+    res.status(404).send('<h1>404 Page</h1>')
 })
 
 app.listen(process.env.PORT)
-
