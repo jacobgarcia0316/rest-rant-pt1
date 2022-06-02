7d98e5325a024630203044cf846467b9a7138fc3
Jacob Garcia <jacobroxasgarcia@yahoo.com>
2022-06-01 22:57:19 -0700

    Navbar with style

--- views/default.jsx
+++ views/default.jsx
@@ -9,6 +9,20 @@ function Def (html) {
                 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"/>
             </head>
             <body>
+            <nav>
+                <ul>
+                    <li>
+                    <a href="/">Home</a>
+                    </li>
+                    <li>
+                    <a href="/places">Places</a>
+                    </li>
+                    <li>
+                    <a href="/places/new">Add Place</a>
+                    </li>
+                </ul>
+            </nav>
+
                 {html.children}
             </body>
         </html>
