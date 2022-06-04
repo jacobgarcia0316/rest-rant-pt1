21fcd892ef6981762a3596ab38f96725e4603e9c
Jacob Garcia <jacobroxasgarcia@yahoo.com>
2022-06-04 16:38:09 -0700

    Show route connected

--- controllers/places.js
+++ controllers/places.js
@@ -5,6 +5,20 @@ router.get('/new',(req, res) => {
   res.render('new')
 })
 
+router.get('/:id', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+    res.render('error404')
+  }
+  else if (!places[id]) {
+    res.render('error404')
+  }
+  else {
+    res.render('../views/show', {place:places[id]})
+  }
+})
+
+
 router.post('/', (req, res) => {
   console.log(req.body)
   if (!req.body.pic) {
