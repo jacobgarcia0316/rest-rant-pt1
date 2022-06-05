d899df069a199d97824080e73e962921439850b3
Jacob Garcia <jacobroxasgarcia@yahoo.com>
2022-06-04 17:28:10 -0700

    Ran through assignment again. Still some bugs that will be worked out in the next few days

--- controllers/places.js
+++ controllers/places.js
@@ -81,5 +81,19 @@ router.get('/:id/edit', (req, res) => {
   }
 })
 
+router.put('/:id', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+      res.render('error404')
+  }
+  else if (!places[id]) {
+      res.render('error404')
+  }
+  else {
+      res.redirect(`/places/${id}`)
+  }
+})
+
+
 
 module.exports = router
